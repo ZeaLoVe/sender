858847eb92bce8b9b4e25f3ceb720c0768464540
ZeaLoVe <ruanguiliang_34@whu.edu.cn>
2015-09-18 14:02:18 +0800

    add mail send function

--- model/model.go
+++ model/model.go
@@ -15,6 +15,27 @@ type Mail struct {
 	Content string `json:"content"`
 }
 
+func (mail *Mail) Msg() string {
+	return fmt.Sprintf("To: %s\r\nSubject: %s\r\n\r\n%s\r\r", mail.Tos, mail.Subject, mail.Content)
+}
+
+func (mail *Mail) TosList() []string {
+	list := strings.Split(mail.Tos, ",")
+	return list
+}
+
+type ACount struct {
+	Server   string
+	User     string
+	Password string
+}
+
+func SendMail(mail *Mail, acount *ACount) error {
+	auth := smtp.PlainAuth("", acount.User, acount.Password, acount.Server)
+	err := smtp.SendMail(acount.Server+":25", auth, acount.User, mail.TosList(), []byte(mail.Msg()))
+	return err
+}
+
 func (this *Sms) String() string {
 	return fmt.Sprintf(
 		"<Tos:%s, Content:%s>",
